7e5a82097cf0d7ae5be880126354d78be682f383
Axel <axosar@gmail.com>
2019-08-12 13:28:41 -0500

    GetJokes function
    get jokes into its function…

--- src/JokeList.js
+++ src/JokeList.js
@@ -11,10 +11,15 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: []
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
     }
   }
-  async componentDidMount(){
+  componentDidMount(){
+    if(this.state.jokes.length === 0){
+      this.getJokes()
+    }
+  }
+  async getJokes(){
     let jokes = [];
     while(jokes.length < this.props.numJokesToGet){
       let res = await axios.get("https://icanhazdadjoke.com/", {
@@ -25,6 +30,10 @@ class JokeList extends Component {
     this.setState({
       jokes: jokes
     });
+    window.localStorage.setItem(
+      "jokes",
+      JSON.stringify(jokes)
+    );
   }
   handleVote(id,delta){
     this.setState(
